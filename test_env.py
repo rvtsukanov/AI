48fb1da8b533b756b7dee9db79cddb072adaaf54
Roman <rvtsukanov@edu.hse.ru>
2018-03-22 22:28:20 +0300

    upd

--- test_env.py
+++ test_env.py
@@ -93,23 +93,4 @@ plt.show()
 print(successes)
 
 
-'''
-from random import randint
-
-dims = 8
-pos = randint(0, dims - 1)
-
-logits = tf.random_uniform([dims], maxval=3, dtype=tf.float32)
-labels = tf.one_hot(pos, dims)
-
-res1 = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
-res2 = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=tf.constant(pos))
-
-with tf.Session() as sess:
-    a, b, logs, labs = sess.run([res1, res2, logits, labels])
-    print(a, b, logs, labs)
-    print(a == b)
-'''
-
-
 
