d349723ee9832a46e0f385dd2e3ab443eefdbca8
Roman <rvtsukanov@edu.hse.ru>
2018-04-11 00:15:13 +0300

    not well working TRPO

--- trpo.py
+++ trpo.py
@@ -5,6 +5,14 @@ import gym
 import matplotlib.pyplot as plt
 from arm_env import ArmEnv
 
+KL_delta = 0.1
+penalty = 10
+
+
+def kl(p, q):
+    return tf.multiply(p, tf.log(p/q))
+
+
 def to_cat(a, n):
     return np.array([1 if a == i else 0 for i in range(n)])
 
@@ -19,7 +27,7 @@ def discount_and_norm_rewards(episode_rewards, gamma):
 
 
 env = ArmEnv(size_x=4, size_y=3, cubes_cnt=4, episode_max_length=2000, finish_reward=200,
-             action_minus_reward=0.0, tower_target_size=3) #is 0.1 IMPORTANT???????????
+             action_minus_reward=0.0, tower_target_size=3)
 
 s = env.reset()
 obs_len = len(s)
@@ -59,14 +67,16 @@ soft_out = tf.nn.softmax(out)
 nl = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=out, labels=actions)
 wnl = tf.multiply(nl, q_estimation)
 loss = tf.reduce_mean(wnl)
-opt = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss) #sometimes GDO works better (dont know why)
+opt = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+
+#########################
+
 
 '''
 =======================
 Critic = Approximation of Q-function
 =======================
 '''
-#q_action = tf.placeholder('int32')  # do we need different action nodes?
 q_return = tf.placeholder('float32', name="Q-Return")  # sum of rewards on rest of traj
 q_inp = tf.layers.dense(
     tf.concat(state, actions),
@@ -88,9 +98,9 @@ q_loss = tf.losses.mean_squared_error(q_out, q_return)
 q_opt = tf.train.AdamOptimizer(0.01).minimize(q_loss)
 
 '''
-============================
+=====================================
 Value-function approximation (linear)
-============================
+=====================================
 '''
 
 value = tf.layers.dense(
@@ -100,9 +110,19 @@ value = tf.layers.dense(
     kernel_initializer=tf.random_normal_initializer(mean=0, stddev=0.1),
     bias_initializer=tf.initializers.constant(0)
 )
-#to learn v-function we use MSE
+#approximate with one-layer NN (linear combination of states)
+
+v_loss = tf.losses.mean_squared_error(value, q_return)
+v_opt = tf.train.AdamOptimizer(0.01).minimize(v_loss)
 
 
+################################
+
+A = q_out - value
+current_policy = tf.placeholder('float32')
+trpo_loss = 1./(obs_len * env.action_space.n) * soft_out/current_policy * A + \
+            penalty * (1./obs_len * kl(soft_out, current_policy) - KL_delta)     #does it work with A?
+trpo_opt = tf.train.AdamOptimizer(0.01).minimize(trpo_loss)
 
 
 '''
@@ -112,15 +132,20 @@ HyperParams
 '''
 num_episodes = 10000
 num_steps = 200
+trpo_steps = 5
 gamma = 1.0
 
 successes = []
 success_episodes = []
 slice = 100
 success_counter = 0
-#q_approximated = 0
 learn_flag = False
 
+'''
+=======================
+Main
+=======================
+'''
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for episode in range(num_episodes):
@@ -139,7 +164,7 @@ with tf.Session() as sess:
                 print("probs: ", probs, learn_flag)
             new_state, reward, done, _ = env.step(a)
             total_rew.append(reward)
-            trajectory.append((s, a, reward)) #no problems?
+            trajectory.append((s, a, reward))
 
             if done:
                 if reward != 0:
@@ -157,8 +182,11 @@ with tf.Session() as sess:
                 ss = st[0]
                 aa = int(st[1])
                 qq = disc[n]
-                q_approximated, _ = sess.run([q_out, q_opt], feed_dict={state: [ss], actions: [aa], q_return: qq})
-                sess.run([opt], feed_dict={state: [ss], actions: [aa], q_estimation: q_approximated})
+                q_approximated, val, _, _ = sess.run([q_out, value, v_opt, q_opt], feed_dict={state: [ss], actions: [aa], q_return: qq})
+                soft, _ = sess.run([soft_out, opt], feed_dict={state: [ss], actions: [aa], q_estimation: q_approximated})
+                for j in range(trpo_steps):
+                    sess.run([trpo_opt],
+                             feed_dict={state: [ss], actions: [aa], q_estimation: q_approximated, current_policy: [soft]})
 
             if episode % slice == 0:
                 print("Episode: ", episode)
